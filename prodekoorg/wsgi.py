1652610e48a38a288b8844f3761f8ee7f70d106c
Santeri Kivinen <skivinen+github@gmail.com>
2018-02-17 20:14:56 +0200

    wsgi säätö

--- prodekoorg/wsgi.py
+++ prodekoorg/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, root)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prodekoorg.settings")
 
 application = get_wsgi_application()
