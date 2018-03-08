43dcebe3168a03d1d7c7e860ad59756af8f6e6c5
rriski <timo.h.riski@outlook.com>
2018-03-08 18:50:02 +0000

    Fix collect-static folder

--- prodekoorg/settings.py
+++ prodekoorg/settings.py
@@ -44,12 +44,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'prodekoorg', 'static')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'prodekoorg', 'collected-static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'prodekoorg', 'collected-static'),
+    os.path.join(BASE_DIR, 'prodekoorg', 'static'),
 )
 SITE_ID = 1
 
