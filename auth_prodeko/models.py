4c42928b40896d5a28478a47e8192777e8148654
rriski <timo@totaltrivia.com>
2018-03-17 15:09:21 +0100

    Modify prodeko network images on footer
    Handle scaling on mobile, remove storaenso and add smartly.io

--- auth_prodeko/models.py
+++ auth_prodeko/models.py
@@ -1,5 +1,3 @@
-"""Declare models for YOUR_APP app."""
-
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
