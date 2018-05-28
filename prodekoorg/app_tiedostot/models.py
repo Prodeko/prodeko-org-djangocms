bccf25e5d05244d6d3ddfd5389003ed0788a814e
Kiikkilä Olli <olli.kiikkila@aalto.fi>
2018-05-28 23:35:16 +0300

    Tiedostot frontend

--- prodekoorg/app_tiedostot/models.py
+++ prodekoorg/app_tiedostot/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 from django.core.files.storage import FileSystemStorage
 import os
 
@@ -8,6 +10,7 @@ destination = FileSystemStorage(location='/files')
 
 class Tiedosto(models.Model):
     file = models.FileField(unique=True, upload_to = "files")
+    thumbnail_image = models.ImageField(upload_to = "file_thumbnail_images", null=True)
     description = models.TextField(blank=True)
     created_date = models.DateField(auto_now_add=True, auto_now=False)
     modified_date = models.DateField(auto_now=True)
@@ -15,3 +18,10 @@ class Tiedosto(models.Model):
     def file_name(self):
         basename, extension = os.path.splitext(os.path.basename(self.file.name))
         return basename
+
+    def file_extension(self):
+        basename, extension = os.path.splitext(os.path.basename(self.file.name))
+        return extension
+
+    def get_thumbnail_image(self):
+        return self.thumbnail_image.url
