5c3160f62f708f5e756b0be0d11b0aa1d4ea0c98
Kiikkilä Olli <olli.kiikkila@aalto.fi>
2018-06-22 01:15:21 +0300

    Tiedostot finishing touches

--- prodekoorg/app_tiedostot/admin.py
+++ prodekoorg/app_tiedostot/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Tiedosto
+from .models import Tiedosto, TiedostoVersio
 
 # Register your models here.
 
-
 admin.site.register(Tiedosto)
+admin.site.register(TiedostoVersio)
