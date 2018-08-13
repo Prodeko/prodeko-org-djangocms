399f01514be2160c5ca6fbea2d957bbb7eb50d79
Kalle H <kalle.hiltunen@outlook.com>
2018-08-13 20:36:14 +0300

    Update Toimari model

--- prodekoorg/app_toimarit/models.py
+++ prodekoorg/app_toimarit/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import *
 
 
 class Toimari(models.Model):
@@ -9,7 +10,7 @@ class Toimari(models.Model):
     virka = models.CharField(max_length=50)
     jaosto = models.CharField(max_length=100)
     # Vain hallituslaisille
-    virka_eng = models.CharField(max_length=30, blank=True)
+    virka_eng = models.CharField(max_length=60, blank=True)
     puhelin = models.CharField(max_length=20, blank=True)
     sahkoposti = models.CharField(max_length=30, blank=True)
 
@@ -17,8 +18,12 @@ class Toimari(models.Model):
     def name(self):
         return '%s %s' % (self.etunimi, self.sukunimi)
 
+    def renderImage(self):
+        return format_html("<img src={} />", "https://www.prodeko.org/wp-content/uploads/2017/12/anonyyyymi-uniseksi-maskulinoitu-150x150.jpg")
+
     def __str__(self):
         return self.name + ", " + self.virka
 
     class Meta:
         verbose_name_plural = "toimarit"
+
