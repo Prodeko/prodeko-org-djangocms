9147c6367aec1c89bbe53cdfed21cdfd154ac0d3
rriski <timo.h.riski@outlook.com>
2018-12-22 19:37:58 +0200

    Modify english translation and add comments

--- prodekoorg/app_kulukorvaus/models.py
+++ prodekoorg/app_kulukorvaus/models.py
@@ -1,21 +1,7 @@
-import os
-
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
-
-def validate_file_extension(value):
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.png', '.jpg', '.jpeg']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(u'Kuitin on oltava .png, .jpg tai .jpeg muodossa.')
-
-
-def validate_iban(self):
-    pass
 
 
 class KulukorvausPerustiedot(models.Model):
@@ -39,6 +25,8 @@ class KulukorvausPerustiedot(models.Model):
 
     id = models.AutoField(primary_key=True)
     # Used to track which User created the kulukorvaus
+    # on_delete=models.CASCADE means that if a user is deleted
+    # all of their reimbursements are deleted as well
     created_by_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     created_by = models.CharField(max_length=50, verbose_name=_('Name'))
     email = models.EmailField(verbose_name=_('Email'))
@@ -72,11 +60,13 @@ class Kulukorvaus(models.Model):
     """
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
-    info = models.ForeignKey(KulukorvausPerustiedot, models.SET_NULL, blank=True, null=True)
+    # on_delete=models.CASCADE means that if a KulukorvausPerustiedot
+    # which is a foreign key of this model is deleted, then this model is deleted also
+    info = models.ForeignKey(KulukorvausPerustiedot, models.CASCADE, blank=True, null=True)
     target = models.CharField(max_length=50, verbose_name=_('Expense explanation'))
     explanation = models.CharField(max_length=100, verbose_name=_('Event / expense target'))
     sum_euros = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Sum (in €)'))
-    additional_info = models.TextField(blank=True, verbose_name=_('Additional info'))
+    additional_info = models.TextField(blank=True, verbose_name=_('Additional information'))
     receipt = models.FileField(upload_to='kulukorvaukset/%Y-%m', verbose_name=_('Receipt'),
                                validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
 
