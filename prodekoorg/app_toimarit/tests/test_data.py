38f79bf4689a81b42d49b3e407c4fb91a049319e
kalh <kalle.hiltunen@outlook.com>
2018-12-26 19:23:34 +0200

    Add model tests to app_toimarit

---
+++ prodekoorg/app_toimarit/tests/test_data.py
@@ -0,0 +1,43 @@
+import tempfile
+from unittest.mock import MagicMock
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.files import File
+from django.test import TestCase
+
+from ..models import Jaosto, Toimari, HallituksenJasen
+
+
+class TestData(TestCase):
+    """Common test data for app_toimarit used across
+    test_forms.py, test_models.py and test_views.py
+
+    Args:
+        TestCase: https://docs.djangoproject.com/en/dev/topics/testing/tools/#testcase.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+
+        User = get_user_model()
+        cls.test_user1 = User.objects.create_user(email='test1@test.com', password='Ukc55Has-@')
+        cls.test_user1.is_staff = True
+        cls.test_user1.save()
+        cls.test_user2 = User.objects.create_user(email='test2@test.com', password='q"WaXkcB>7')
+
+        cls.test_jaosto1 = Jaosto.objects.create(name='Test1')
+        cls.test_jaosto2 = Jaosto.objects.create(name='Test2')
+
+        cls.test_toimari1 = Toimari.objects.create(firstname='John', 
+                                                  lastname='Doe', 
+                                                  position='Webbitiimi', 
+                                                  section=cls.test_jaosto1)
+
+        cls.test_hallituksenjasen1 = HallituksenJasen.objects.create(firstname='John', 
+                                                                    lastname='Doe', 
+                                                                    position='Puheenjohtaja', 
+                                                                    position_eng='Chairman', 
+                                                                    section=cls.test_jaosto2, 
+                                                                    mobilephone='000123123', 
+                                                                    email='puheenjohtaja@')
---
+++ prodekoorg/app_toimarit/tests/test_models.py
@@ -0,0 +1,142 @@
+from django.test import override_settings
+
+from .test_data import TestData
+
+# Override settigs to test translations
+english = override_settings(
+    LANGUAGE_CODE='en',
+    LANGUAGES=(('en', 'English'),),
+)
+
+finnish = override_settings(
+    LANGUAGE_CODE='fi',
+    LANGUAGES=(('fi', 'Finnish'),),
+)
+
+
+class JaostoModelTest(TestData):
+    """Tests for Jaosto model."""
+
+    @english
+    def test_name_label_english(self):
+        field_label = self.test_jaosto1._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'Name')
+
+    @finnish
+    def test_name_label_finnish(self):
+        field_label = self.test_jaosto1._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'Nimi')
+
+    def test_name_max_length(self):
+        max_length = self.test_jaosto1._meta.get_field('name').max_length
+        self.assertEquals(max_length, 50)
+
+class ToimariModelTest(TestData):
+    """Tests for Toimari model."""
+
+    @english
+    def test_firstname_label_english(self):
+        field_label = self.test_toimari1._meta.get_field('firstname').verbose_name
+        self.assertEquals(field_label, 'First name')
+
+    @finnish
+    def test_firstname_label_finnish(self):
+        field_label = self.test_toimari1._meta.get_field('firstname').verbose_name
+        self.assertEquals(field_label, 'Etunimi')
+
+    def test_firstname_max_length(self):
+        max_length = self.test_toimari1._meta.get_field('firstname').max_length
+        self.assertEquals(max_length, 30)
+
+    @english
+    def test_lastname_label_english(self):
+        field_label = self.test_toimari1._meta.get_field('lastname').verbose_name
+        self.assertEquals(field_label, 'Last name')
+
+    @finnish
+    def test_lastname_label_finnish(self):
+        field_label = self.test_toimari1._meta.get_field('lastname').verbose_name
+        self.assertEquals(field_label, 'Sukunimi')
+
+    def test_lastnamename_max_length(self):
+        max_length = self.test_toimari1._meta.get_field('lastname').max_length
+        self.assertEquals(max_length, 30)
+
+    @english
+    def test_position_label_english(self):
+        field_label = self.test_toimari1._meta.get_field('position').verbose_name
+        self.assertEquals(field_label, 'Position')
+
+    @finnish
+    def test_position_label_finnish(self):
+        field_label = self.test_toimari1._meta.get_field('position').verbose_name
+        self.assertEquals(field_label, 'Virka')
+
+    def test_position_max_length(self):
+        max_length = self.test_toimari1._meta.get_field('position').max_length
+        self.assertEquals(max_length, 50)
+
+    @english
+    def test_section_label_english(self):
+        field_label = self.test_toimari1._meta.get_field('section').verbose_name
+        self.assertEquals(field_label, 'Section')
+
+    @finnish
+    def test_section_label_finnish(self):
+        field_label = self.test_toimari1._meta.get_field('section').verbose_name
+        self.assertEquals(field_label, 'Jaosto')
+
+class HallituksenJasenModelTest(TestData):
+    """Tests for HallituksenJasen model."""
+
+    @english
+    def test_firstname_label_english(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('firstname').verbose_name
+        self.assertEquals(field_label, 'First name')
+
+    @finnish
+    def test_firstname_label_finnish(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('firstname').verbose_name
+        self.assertEquals(field_label, 'Etunimi')
+
+    def test_firstname_max_length(self):
+        max_length = self.test_hallituksenjasen1._meta.get_field('firstname').max_length
+        self.assertEquals(max_length, 30)
+
+    @english
+    def test_lastname_label_english(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('lastname').verbose_name
+        self.assertEquals(field_label, 'Last name')
+
+    @finnish
+    def test_lastname_label_finnish(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('lastname').verbose_name
+        self.assertEquals(field_label, 'Sukunimi')
+
+    def test_lastnamename_max_length(self):
+        max_length = self.test_hallituksenjasen1._meta.get_field('lastname').max_length
+        self.assertEquals(max_length, 30)
+
+    @english
+    def test_position_label_english(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('position').verbose_name
+        self.assertEquals(field_label, 'Position')
+
+    @finnish
+    def test_position_label_finnish(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('position').verbose_name
+        self.assertEquals(field_label, 'Virka')
+
+    def test_position_max_length(self):
+        max_length = self.test_hallituksenjasen1._meta.get_field('position').max_length
+        self.assertEquals(max_length, 50)
+
+    @english
+    def test_section_label_english(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('section').verbose_name
+        self.assertEquals(field_label, 'Section')
+
+    @finnish
+    def test_section_label_finnish(self):
+        field_label = self.test_hallituksenjasen1._meta.get_field('section').verbose_name
+        self.assertEquals(field_label, 'Jaosto')
