07d3c5a5be96a833a97124744357129b734624aa
kalh <kalle.hiltunen@outlook.com>
2018-12-28 18:07:00 +0200

    Add translations to edit profile page

--- auth_prodeko/forms.py
+++ auth_prodeko/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.password_validation import get_default_password_validators
 from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 class EditProfileForm(forms.Form):
     email = forms.EmailField(label='Email', max_length=50, required=False)
@@ -19,7 +20,7 @@ class EditProfileForm(forms.Form):
     def clean_password(self):
         password = self.cleaned_data.get("password")
         if not check_password(password, self.user.password):
-            self.add_error('password', "Incorrect password")
+            self.add_error('password', _("Incorrect password"))
 
     def clean(self):
         cleaned_data = super().clean()
@@ -38,5 +39,5 @@ class EditProfileForm(forms.Form):
 	        	self.add_error('newpassword', error)
 
 	        if newpassword_confirm != newpassword:
-	            self.add_error('newpassword', "Passwords don't match")
-	            self.add_error('newpassword_confirm', "Passwords don't match")
+	            self.add_error('newpassword', _("Passwords don't match"))
+	            self.add_error('newpassword_confirm', _("Passwords don't match"))
