c55b40ec4591a6c22e1189ab14113a72d44191fd
kalh <kalle.hiltunen@outlook.com>
2018-12-30 12:18:28 +0200

    Add apphooks to apps

---
+++ prodekoorg/app_apply_for_membership/cms_apps.py
@@ -0,0 +1,16 @@
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
+from .views import main_form
+
+
+@apphook_pool.register
+class ApplyForMembershipApphook(CMSApp):
+    app_name = "app_apply_for_membership"
+    name = _("Apply For Membership")
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        return [
+            url(r'^$', main_form)
+        ]
