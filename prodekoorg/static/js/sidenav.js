367513e95c3d09aaa648e892749ce75fc31d97ef
petrus <holm.petrus@gmail.com>
2019-02-11 15:10:41 +0200

    better dimensions for sidenav, more smooth scroll

--- prodekoorg/static/js/sidenav.js
+++ prodekoorg/static/js/sidenav.js
@@ -5,9 +5,16 @@ $(document).scroll(function() {
 function checkOffset() {
   if (
     $('#sidenav').offset().top + $('#sidenav').height() >=
-    $('#footer').offset().top - 10
-  )
+    $('#footer').offset().top - 100
+  ) {
     $('#sidenav').css('position', 'absolute');
-  if ($(document).scrollTop() + window.innerHeight < $('#footer').offset().top)
+    $('#sidenav').css('bottom', '0px');
+  }
+  if (
+    $(document).scrollTop() + window.innerHeight <
+    $('#footer').offset().top
+  ) {
     $('#sidenav').css('position', 'fixed'); // restore when you scroll up
+    $('#sidenav').css('bottom', '40px');
+  }
 }
