1bca2c49555ae127961b98ee01b14156c9f2a5d0
rriski <timo.h.riski@outlook.com>
2019-02-22 00:07:02 +0200

    Sort imports

--- auth_prodeko/models.py
+++ auth_prodeko/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
-
 from prodekoorg.app_apply_for_membership.models import PendingUser
 
 
