d85eeaa2b2f0df38ab2a3f82087e37baab00a2a6
Julius Jaskari <julius.jaskari@gmail.com>
2019-03-28 14:03:00 +0200

    instagram

---
+++ prodekoorg/static/js/instagram.js
@@ -0,0 +1,14 @@
+const ACCESS_TOKEN = "2398789603.1677ed0.2175ddadc380435f9a837eac1a212d0c";
+var num = 4
+if(window.innerWidth >= 576) {
+   num = 8;
+}
+window.onload = fetch(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${ACCESS_TOKEN}&count=${num}`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        //console.log(JSON.stringify(myJson));
+        console.log(JSON.stringify(myJson.data));
+        myJson.data.map(image => $('div#instagram').append(`<a href=${image.link} target= "_blank"><img src=${image.images.low_resolution.url}></img></a>`));
+    });
