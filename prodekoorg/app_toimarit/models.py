ba6fbe2a3fc633097b0d3c4c26fdf14a47737884
kalh <kalle.hiltunen@outlook.com>
2019-06-26 22:30:31 +0300

    Fix images appearing small

--- prodekoorg/app_toimarit/models.py
+++ prodekoorg/app_toimarit/models.py
@@ -3,7 +3,6 @@ import os.path
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-
 class Jaosto(models.Model):
     """Prodeko board proceedings documents.
 
@@ -111,7 +110,7 @@ class HallituksenJasen(models.Model):
         )
 
     def photourl(self):
-        if os.path.isfile("static/images/hallitus_photos/{}_{}.jpg".format(self.firstname, self.lastname)):
+        if os.path.isfile("prodekoorg/app_toimarit/static/images/hallitus_photos/{}_{}.jpg".format(self.firstname, self.lastname)):
             return "%s_%s.jpg" % (self.firstname, self.lastname)
         else:
             return 'placeholder.jpg'
