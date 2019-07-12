f61e6df0b47f44e9dce14bf6e4823ab3549f3b9b
rriski <timo.h.riski@outlook.com>
2019-07-12 21:57:29 +0300

    Add server time to infoscreen api

--- prodekoorg/app_infoscreen/urls.py
+++ prodekoorg/app_infoscreen/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, re_path
-from .views_api import SlidesList
+from .views_api import SlidesList, now
 
 app_name = "app_infoscreen"
-urlpatterns = [re_path(r"^api/slides/$", SlidesList.as_view())]
+urlpatterns = [
+    re_path(r"^api/slides/$", SlidesList.as_view()),
+    re_path(r"^api/time/$", now),
+]
--- prodekoorg/app_infoscreen/views_api.py
+++ prodekoorg/app_infoscreen/views_api.py
@@ -1,6 +1,11 @@
+import datetime
+import json
+
+from django.http import HttpResponse
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import Slide
 
 
@@ -18,3 +23,9 @@ class SlidesList(APIView):
         active_slides = [s for s in queryset if s.is_active()]
         serializer = SlideSerializer(active_slides, many=True)
         return Response(serializer.data)
+
+
+def now(request):
+    """ Return current time"""
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return HttpResponse(json.dumps(now), content_type="application/json")
