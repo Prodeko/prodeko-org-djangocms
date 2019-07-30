2732cff9625d22865a494c70f0905fb75a601f2f
rriski <timo.h.riski@outlook.com>
2019-07-31 00:42:36 +0300

    Linted alumnirekisteri, hopefully nothing broke

--- alumnirekisteri/rekisteri/static/js/admin.js
+++ alumnirekisteri/rekisteri/static/js/admin.js
@@ -1,99 +1,97 @@
 /* Get csrf_token */
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + '=') {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 var csrftoken = getCookie('csrftoken');
 
 /* make-admin post */
-$(".make-admin").click(function(){
-  var user_id = $(this).attr("user-id");
+$('.make-admin').click(function() {
+  var user_id = $(this).attr('user-id');
   var post_data = {
-    'csrfmiddlewaretoken': csrftoken,
-    'action': 'make-admin',
-    'user_id': user_id,
-  }
-  $.post( "/admin/", post_data, function( data ) {
+    csrfmiddlewaretoken: csrftoken,
+    action: 'make-admin',
+    user_id: user_id
+  };
+  $.post('/admin/', post_data, function() {
     location.reload();
   });
 });
 
 /* make-user post */
-$(".make-user").click(function(){
-  var user_id = $(this).attr("user-id");
+$('.make-user').click(function() {
+  var user_id = $(this).attr('user-id');
   var post_data = {
-    'csrfmiddlewaretoken': csrftoken,
-    'action': 'make-user',
-    'user_id': user_id,
-  }
-  $.post( "/admin/", post_data, function( data ) {
+    csrfmiddlewaretoken: csrftoken,
+    action: 'make-user',
+    user_id: user_id
+  };
+  $.post('/admin/', post_data, function() {
     location.reload();
   });
 });
 
 /* make-inactive post */
-$(".make-inactive").click(function(){
-  var user_id = $(this).attr("user-id");
+$('.make-inactive').click(function() {
+  var user_id = $(this).attr('user-id');
   var post_data = {
-    'csrfmiddlewaretoken': csrftoken,
-    'action': 'make-inactive',
-    'user_id': user_id,
-  }
-  $.post( "/admin/", post_data, function( data ) {
+    csrfmiddlewaretoken: csrftoken,
+    action: 'make-inactive',
+    user_id: user_id
+  };
+  $.post('/admin/', post_data, function() {
     location.reload();
   });
 });
 
 /* make-hidden post */
-$(".make-hidden").click(function(){
-  var user_id = $(this).attr("user-id");
+$('.make-hidden').click(function() {
+  var user_id = $(this).attr('user-id');
   var post_data = {
-    'csrfmiddlewaretoken': csrftoken,
-    'action': 'make-hidden',
-    'user_id': user_id,
-  }
-  $.post( "/admin/", post_data, function( data ) {
+    csrfmiddlewaretoken: csrftoken,
+    action: 'make-hidden',
+    user_id: user_id
+  };
+  $.post('/admin/', post_data, function() {
     location.reload();
   });
 });
-
 
 /* delete-user post */
 function deleteUser(user_id) {
   var post_data = {
-    'csrfmiddlewaretoken': csrftoken,
-    'action': 'delete-user',
-    'user_id': user_id,
-  }
-  $.post( "/admin/", post_data, function( data ) {
+    csrfmiddlewaretoken: csrftoken,
+    action: 'delete-user',
+    user_id: user_id
+  };
+  $.post('/admin/', post_data, function() {
     location.reload();
   });
 }
 
-
 /* delete user popup */
-$(".delete-user-button").click(function(){
-  var user_id = $(this).attr("user-id");
-  var user_name = $(this).attr("user-name");
-  $("#delete-user-modal").modal('show');
-  $("#delete-user-modal #modal-user-name").html(user_name);
-  $("#delete-user-modal #delete-user").attr("user-id", user_id);
+$('.delete-user-button').click(function() {
+  var user_id = $(this).attr('user-id');
+  var user_name = $(this).attr('user-name');
+  $('#delete-user-modal').modal('show');
+  $('#delete-user-modal #modal-user-name').html(user_name);
+  $('#delete-user-modal #delete-user').attr('user-id', user_id);
 });
 
 /* confirm delete user */
-$("#delete-user-modal #delete-user").click(function(){
-  var user_id = $(this).attr("user-id");
+$('#delete-user-modal #delete-user').click(function() {
+  var user_id = $(this).attr('user-id');
   deleteUser(user_id);
   //location.reload();
 });
--- alumnirekisteri/rekisteri/static/js/myprofile.js
+++ alumnirekisteri/rekisteri/static/js/myprofile.js
@@ -1,122 +1,133 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
   /* get profile image data */
-  imageData = {data:null};
-  SimpleCrop($("#crop")[0], {
-      confirmButton: $("#crop-save-image")[0],
-      outputImage: $("#crop-image")[0],
-      outputData: imageData,
-      outputWidth: 1024,
-      width: 512,
-      height: 512
+  var imageData = { data: null };
+  SimpleCrop($('#crop')[0], {
+    confirmButton: $('#crop-save-image')[0],
+    outputImage: $('#crop-image')[0],
+    outputData: imageData,
+    outputWidth: 1024,
+    width: 512,
+    height: 512
   });
 
   /* Submit main-form */
-  $("#main-form").on("submit", function(event){
+  $('#main-form').on('submit', function(event) {
     event.preventDefault();
-    $(".error").remove();
+    $('.error').remove();
     var form = $(this);
     var formData = new FormData(form[0]);
-    formData.append("crop_image_data", imageData.data);
-    var request = $.ajax({
-        url: form.attr("action"),
-        data: formData,
-        type: 'POST',
-        async: true,
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: function(response){
-          if (response.success) {
-            location.reload();
-          } else {
-
-            for (var key in response.errors.user_form){
-              var errorElement = '<div class="error alert alert-danger">'+response.errors.user_form[key]+'</div>'
-              $('input[name=user_form-'+key+']').after(errorElement);
-            }
-            for (var key in response.errors.person_form){
-              var errorElement = '<div class="error alert alert-danger">'+response.errors.person_form[key]+'</div>'
-              $('input[name=person_form-'+key+']').after(errorElement);
-            }
-
+    formData.append('crop_image_data', imageData.data);
+    $.ajax({
+      url: form.attr('action'),
+      data: formData,
+      type: 'POST',
+      async: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(response) {
+        if (response.success) {
+          location.reload();
+        } else {
+          for (var key in response.errors.user_form) {
+            var errorElement =
+              '<div class="error alert alert-danger">' +
+              response.errors.user_form[key] +
+              '</div>';
+            $('input[name=user_form-' + key + ']').after(errorElement);
+          }
+          for (key in response.errors.person_form) {
+            errorElement =
+              '<div class="error alert alert-danger">' +
+              response.errors.person_form[key] +
+              '</div>';
+            $('input[name=person_form-' + key + ']').after(errorElement);
           }
         }
+      }
     });
     return false;
   });
 
   /* All forms that are inside a modal with class modal-form
     will be submitted by this function. */
-  $(".modal-form").on("submit", "form", function(event){
+  $('.modal-form').on('submit', 'form', function(event) {
     event.preventDefault();
-    $(".error").remove();
-    var response = submitForm($(this).id, $(this).attr("action"), $(this).serialize());
+    $('.error').remove();
+    var response = submitForm($(this).attr('action'), $(this).serialize());
     if (response.success) {
       location.reload();
     } else {
       for (var key in response.errors) {
-        var errorElement = '<div class="error alert alert-danger">'+response.errors[key]+'</div>'
-        $('input[name='+key+']').after(errorElement);
-        $('select[name='+key+']').after(errorElement);
+        var errorElement =
+          '<div class="error alert alert-danger">' +
+          response.errors[key] +
+          '</div>';
+        $('input[name=' + key + ']').after(errorElement);
+        $('select[name=' + key + ']').after(errorElement);
       }
-    };
+    }
     return false;
   });
 
   /* get edit modal contents */
-  $(".edit-link").on("click", function(event){
+  $('.edit-link').on('click', function(event) {
     event.preventDefault();
-    var modal = $($(this).attr("modal"));
-    var url = $(this).attr("url")
-    $.get( url, function( data ) {
-      modal.find(".modal-content").html( data );
+    var modal = $($(this).attr('modal'));
+    var url = $(this).attr('url');
+    $.get(url, function(data) {
+      modal.find('.modal-content').html(data);
     });
-    modal.modal("show");
+    modal.modal('show');
   });
 
   /* get delete modal contents */
-  $(".delete-link").on("click", function(event){
+  $('.delete-link').on('click', function(event) {
     event.preventDefault();
-    var modal = $($(this).attr("modal"));
-    var url = $(this).attr("url")
-    $.get( url, function( data ) {
-      modal.find(".modal-content").html( data );
+    var modal = $($(this).attr('modal'));
+    var url = $(this).attr('url');
+    $.get(url, function(data) {
+      modal.find('.modal-content').html(data);
     });
-    modal.modal("show");
+    modal.modal('show');
   });
 
-  updateVisibility = function(event){
-      $(".panel:has(.category-checkbox:not(checked))").addClass("category-disabled");
-      $(".panel:has(.category-checkbox:not(checked))").removeClass("category-enabled");
-      $(".panel:has(.category-checkbox:checked)").removeClass("category-disabled");
-      $(".panel:has(.category-checkbox:checked)").addClass("category-enabled");
-  }
-  $(".category-checkbox").on("change", updateVisibility);
+  var updateVisibility = function() {
+    $('.panel:has(.category-checkbox:not(checked))').addClass(
+      'category-disabled'
+    );
+    $('.panel:has(.category-checkbox:not(checked))').removeClass(
+      'category-enabled'
+    );
+    $('.panel:has(.category-checkbox:checked)').removeClass(
+      'category-disabled'
+    );
+    $('.panel:has(.category-checkbox:checked)').addClass('category-enabled');
+  };
+  $('.category-checkbox').on('change', updateVisibility);
 
   updateVisibility();
 });
 
-
 /* submit the form, having the given formId as id attribute,
     without reloading the page */
-function submitForm(formId, url, data){
-  var form = document.getElementById(formId);
-  var responseText;
+function submitForm(url, data) {
   var jqXHR = $.ajax({
-    type: "POST",
+    type: 'POST',
     url: url,
     data: data,
-    dataType: "json",
+    dataType: 'json',
     async: false
   });
   return jqXHR.responseJSON;
 }
 
 /* remove error messages when modal closes */
-$('.modal').on('hidden.bs.modal', function () {
-    $(".error").remove();
-})
+$('.modal').on('hidden.bs.modal', function() {
+  $('.error').remove();
+});
 
 /* hide messages after 5 seconds */
-$(".footer .alert").delay(5000).fadeOut('slow');
+$('.footer .alert')
+  .delay(5000)
+  .fadeOut('slow');
