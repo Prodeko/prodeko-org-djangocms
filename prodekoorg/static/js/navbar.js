4724861e3d36ff0a069a6da3fc8fa8905092f174
rriski <timo.h.riski@outlook.com>
2019-08-28 22:57:39 +0300

    Improve navbar scroll behaviour

--- prodekoorg/static/js/navbar.js
+++ prodekoorg/static/js/navbar.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   var hamburger = $('.hamburger');
+
+  function preventScrolling() {
+    $('body').css('overflow', 'hidden');
+    $('body').css('position', 'fixed');
+  }
+
+  function allowScrolling() {
+    $('body').css('overflow', 'visible');
+    $('body').css('position', 'static');
+  }
+
   hamburger.on('click', function() {
     hamburger.toggleClass('is-active');
+    hamburger.hasClass('is-active') ? preventScrolling() : allowScrolling();
   });
 });
