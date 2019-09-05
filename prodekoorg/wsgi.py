447d2706e70aea049f547a4e2ae6eed82a5dd54c
rriski <timo.h.riski@outlook.com>
2019-09-06 00:54:41 +0300

    Modify wsgi.py to point to correct settings

--- prodekoorg/wsgi.py
+++ prodekoorg/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for prodekoorg project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 root = os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, root)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prodekoorg.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prodekoorg.settings.prod")
 
 application = get_wsgi_application()
