49370d225010a03eca32097e6c773e2f6836fdf6
rriski <timo.h.riski@outlook.com>
2019-09-12 02:52:15 +0300

    Fix 500 error page static files

--- prodekoorg/views.py
+++ prodekoorg/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
 def handler500(request, template_name="500.html"):
-    response = render_to_response("500.html")
+    response = render(requets, "500.html")
     response.status_code = 500
     return response
