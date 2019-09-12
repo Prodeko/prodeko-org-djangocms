fcb53fb6ed8f9e09620fde3dd0db895cc877c957
rriski <timo.h.riski@outlook.com>
2019-09-12 03:29:37 +0300

    fix typo

--- prodekoorg/views.py
+++ prodekoorg/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def handler500(request, template_name="500.html"):
-    response = render(requets, "500.html")
+    response = render(request, "500.html")
     response.status_code = 500
     return response
