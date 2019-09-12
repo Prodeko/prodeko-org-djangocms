d9b5c4a399453652db3d07fa437a2c8f3595fb2c
rriski <timo.h.riski@outlook.com>
2019-09-12 15:11:56 +0300

    Remove localhost from allowed hosts in prod

--- prodekoorg/settings/prod.py
+++ prodekoorg/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["localhost", "prodeko.org", ".prodeko.org"]
+ALLOWED_HOSTS = ["prodeko.org", ".prodeko.org"]
 
 # When DEBUG = False, all errors with level ERROR or
 # higher get mailed to ADMINS according to LOGGING conf
