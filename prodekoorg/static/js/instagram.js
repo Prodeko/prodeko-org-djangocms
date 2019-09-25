2ca420d26e6ed348e3d9d758a325e5971197c646
rriski <timo.h.riski@outlook.com>
2019-09-25 23:18:03 +0300

    Work on frontend
    - Update sidenav and navbar mobile to support pages nested 3 levels deep.
    - Work on instagram widget styling
    - Work on frontpage styling
    - Update <a> tag color to be more clear
    - Make sidenav sticky

--- prodekoorg/static/js/instagram.js
+++ prodekoorg/static/js/instagram.js
@@ -1,16 +1,18 @@
 const ACCESS_TOKEN = '2398789603.1677ed0.0f8cf3e0b2684743a9ea6b9a9636dadb';
 var num = 4;
+
 if (window.innerWidth >= 576) {
-  num = 6;
+  num = 8;
 }
+
 window.onload = fetch(
   `https://api.instagram.com/v1/users/self/media/recent/?access_token=${ACCESS_TOKEN}&count=${num}`
 )
   .then(function(response) {
     return response.json();
   })
-  .then(function(myJson) {
-    myJson.data.map(image =>
+  .then(function(json) {
+    json.data.map(image =>
       $('div#instagram').append(
         `<a href=${image.link} target= "_blank"><img src=${image.images.low_resolution.url}></img></a>`
       )
