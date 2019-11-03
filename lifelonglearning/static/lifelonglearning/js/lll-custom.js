c4b8a1ce477e2ce785bc15456011ff66691df046
rriski <timo.h.riski@outlook.com>
2019-11-03 18:08:38 +0200

    Improve lifelonglearning
    - Fixes for javascript/navigation
    - Fixes for styling

--- lifelonglearning/static/lifelonglearning/js/lll-custom.js
+++ lifelonglearning/static/lifelonglearning/js/lll-custom.js
@@ -1,41 +1,31 @@
 $(document).ready(function() {
   $('#content').on('click', function() {
-    if ($(window).width() < 800 && $('#content').hasClass('blur')) {
+    if ($(window).width() < 992) {
       $('.navbar-toggler').trigger('click');
     }
   });
 
-  // Creates blur effect in the background when the menu icon is clicked on mobile
-  $('.navbar-toggler').on('click', function() {
-    if ($('#content').hasClass('blur')) {
-      $('#content').removeClass('blur');
+  function toggleNavbar() {
+    if ($(window).width() >= 992) {
+      if ($(this).scrollTop() > 50 || $(document).scrollTop() > 50) {
+        $('#lll-navbar').css('opacity', '0.95');
+        $('#scrollToTop').css('opacity', '1');
+      } else {
+        if ($(window).width() >= 992) $('#lll-navbar').css('opacity', '0');
+        if ($(window).width() >= 992) $('#lll-navbar').css('display', 'none');
+        $('#scrollToTop').css('opacity', '0');
+      }
     } else {
-      $('#content').addClass('blur');
+      $('#lll-navbar').css('opacity', '0.95');
     }
-  });
-
-  // Setup by device and scroll: if start position is not on top, create background for navbar
-  if ($(window).width() < 800) $('#lll-navbar').css('opacity', '1');
-  if ($(window).scrollTop() > 50) {
-    $('#navbar').css('background-position', 'left 0px');
-    $('#lll-navbar').css('opacity', '0.8');
-    $('#scrollToTop').css('opacity', '1');
   }
 
-  // Create navbar background when scrolled
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 50 || $(document).scrollTop() > 50) {
-      $('#navbar').css('background-position', 'left 0px');
-      $('#lll-navbar').css('opacity', '0.8');
-      $('#scrollToTop').css('opacity', '1');
-    } else {
-      $('#navbar').css('background-position', 'left -300px');
-      if ($(window).width() > 800) $('#lll-navbar').css('opacity', '0');
-      if ($(window).width() > 800) $('#lll-navbar').css('display', 'none');
-      $('#scrollToTop').css('opacity', '0');
-    }
-  });
+  toggleNavbar();
 
+  // Create navbar background when scrolled
+  $(document).scroll(function() {
+    toggleNavbar();
+  });
   // When header is clicked, scroll to top
   $('#scrollToTop').on('click', function(event) {
     event.preventDefault();
@@ -50,9 +40,6 @@ $(document).ready(function() {
 
   // Select all anchor links
   $(document).on('click', "a[href*='#']", function(event) {
-    if ($(window).width() < 800 && $('#content').hasClass('blur')) {
-      $('.navbar-toggler').trigger('click');
-    }
     if (
       location.pathname.replace(/^\//, '') ==
         this.pathname.replace(/^\//, '') &&
