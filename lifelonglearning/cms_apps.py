ddec481e0dcceb1dfd6638c33354809f352fd6a4
rriski <timo.h.riski@outlook.com>
2019-11-07 22:48:48 +0200

    Fix lifelong learning spelling

--- lifelonglearning/cms_apps.py
+++ lifelonglearning/cms_apps.py
@@ -9,7 +9,7 @@ from .views import coursepage, index
 @apphook_pool.register
 class LifelonglearningApphook(CMSApp):
     app_name = "lifelonglearning"
-    name = _("Lifelonglearning page")
+    name = _("Lifelong learning page")
 
     def get_urls(self, page=None, language=None, **kwargs):
         return [
