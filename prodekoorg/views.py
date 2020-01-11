a3bddd667bcf367e499d53397cd125df13224784
rriski <timo.h.riski@outlook.com>
2020-01-12 00:31:39 +0200

    Fix handler404

--- prodekoorg/views.py
+++ prodekoorg/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sentry_sdk import capture_message
 
 
-def handler404(request, template_name="404.html"):
+def handler404(request, exception=None, template_name="404.html"):
     capture_message("Page not found!", level="error")
     response = render(request, "404.html")
     response.status_code = 404
