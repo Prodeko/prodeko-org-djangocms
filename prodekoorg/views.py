55c04c1e9bcbfbdb3177dbba25f9d7e2cfad8c8b
rriski <timo.h.riski@outlook.com>
2020-01-12 13:13:40 +0200

    Remove sentry 404 error tracking

--- prodekoorg/views.py
+++ prodekoorg/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-from sentry_sdk import capture_message
-
-
-def handler404(request, exception=None, template_name="404.html"):
-    capture_message("Page not found!", level="error")
-    response = render(request, "404.html")
-    response.status_code = 404
-    return response
 
 def handler500(request, template_name="500.html"):
     response = render(request, "500.html")
