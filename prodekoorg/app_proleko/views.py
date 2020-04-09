0282d82f274fa06f9dbe0e3bde654ea8a42958ad
Olli Kiikkilä <kiikkila.olli@gmail.com>
2020-04-09 04:49:13 +0300

    random generated gradients

--- prodekoorg/app_proleko/views.py
+++ prodekoorg/app_proleko/views.py
@@ -3,11 +3,28 @@ from itertools import groupby
 from django.shortcuts import render
 from django.http import HttpResponseBadRequest, JsonResponse
 from .models import Lehti, Post
+import random
+
+COLORS = [
+    "#FF8552", "#E6E6E6", "#FBB13C", "#D81159", "#8F2D56", "#FFDBB5", "#218380", "#B7C3F3", "#CF1259", "#DDE392", "#78E0DC", "#fcba03", "#ff0062", "#ffdd00", "#ff6600", "#0095ff", "#00ff99", "#07d100", "#00e5ff"
+]
 
 
 def posts(request):
     posts = Post.objects.all()
-    return render(request, "posts.html", {"posts": posts})
+    posts_dict = posts.values()
+
+    for post in posts_dict:
+        post["get_thumbnail_image"] = posts.get(
+            pk=post["id"]).get_thumbnail_image()
+        post["total_likes"] = posts.get(pk=post["id"]).total_likes()
+        color1 = random.choice(COLORS)
+        post["color1"] = color1
+        color2 = random.choice(COLORS)
+        while (color2 == color1):
+            color2 = random.choice(COLORS)
+        post["color2"] = color2
+    return render(request, "posts.html", {"posts": posts_dict})
 
 
 def post(request, post_id):
