ccedcf99d9839a4f5be29b5568cc1cbfcb8b5fc2
Timo Riski <timo.h.riski@aalto.fi>
2020-11-30 21:02:07 +0200

    Update tiedotteet dependencies and fix static files

--- tiedotteet/frontend/src/serviceWorker.js
+++ tiedotteet/frontend/src/serviceWorker.js
@@ -1,17 +1,15 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('../static/tiedotteet/serviceworker.js')
-      .then(
-        (registration) => {
-          console.log(
-            'ServiceWorker registration successful with scope: ',
-            registration.scope
-          )
-        },
-        (err) => {
-          console.log('ServiceWorker registration failed: ', err)
-        }
-      )
+    navigator.serviceWorker.register('./assets/serviceworker.js').then(
+      (registration) => {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        )
+      },
+      (err) => {
+        console.log('ServiceWorker registration failed: ', err)
+      }
+    )
   })
 }
