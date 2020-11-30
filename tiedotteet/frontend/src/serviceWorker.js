2a8fb7af9a7f3e45c0af09daf04e899c18d5f4cd
Timo Riski <timo.h.riski@aalto.fi>
2020-11-30 21:50:13 +0200

    More static file fixes

--- tiedotteet/frontend/src/serviceWorker.js
+++ tiedotteet/frontend/src/serviceWorker.js
@@ -1,15 +1,17 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./assets/serviceworker.js').then(
-      (registration) => {
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope
-        )
-      },
-      (err) => {
-        console.log('ServiceWorker registration failed: ', err)
-      }
-    )
+    navigator.serviceWorker
+      .register('./static/tiedotteet/serviceworker.js')
+      .then(
+        (registration) => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          )
+        },
+        (err) => {
+          console.log('ServiceWorker registration failed: ', err)
+        }
+      )
   })
 }
