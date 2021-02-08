b72ac60c66d9ff133fd4ecaf33f53262afc83010
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-02-08 14:46:12 +0200

    Fix tiedotteet api permissions

--- prodekoorg/settings/base.py
+++ prodekoorg/settings/base.py
@@ -404,7 +404,6 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
-        "rest_framework.permissions.IsAuthenticated",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     )
