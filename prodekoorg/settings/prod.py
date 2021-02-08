be6a35d99bf6eed6a49bd27d756c3144b657d73d
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-02-08 10:03:21 +0200

    Add sentry url to CORS_ALLOWED_ORIGINS

--- prodekoorg/settings/prod.py
+++ prodekoorg/settings/prod.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = ["prodeko.org", ".prodeko.org", "prodeko.fi", ".prodeko.fi", "0.
 
 CORS_ALLOWED_ORIGINS = [
     "https://ilmo3.prodeko.org",
+    "https://browser.sentry-cdn.com"
 ]
 
 # When DEBUG = False, all errors with level ERROR or
