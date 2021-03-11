a6cc1912f0cd97e723d9e0c352afe74eac0993fb
rriski <25483483+rriski@users.noreply.github.com>
2021-03-11 23:19:09 +0200

    Add sas_token for accessing Azure storage (#92)

--- prodekoorg/custom_azure.py
+++ prodekoorg/custom_azure.py
@@ -7,6 +7,7 @@ from storages.backends.azure_storage import AzureStorage
 class AzureMediaStorage(AzureStorage):
     account_name = "prodekostorage"
     account_key = settings.STORAGE_KEY
+    sas_token = settings.SAS_TOKEN
     azure_container = "media"
     expiration_secs = None
     # This is needed for django-filer
@@ -16,6 +17,7 @@ class AzureMediaStorage(AzureStorage):
 class AzureStaticStorage(ManifestFilesMixin, AzureStorage):
     account_name = "prodekostorage"
     account_key = settings.STORAGE_KEY
+    sas_token = settings.SAS_TOKEN
     azure_container = "static"
     expiration_secs = None
 
