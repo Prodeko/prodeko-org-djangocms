5e814bde8aa6e5cd7ca1ccefff8ae5e7a85d88ee
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-08-31 17:06:21 +0300

    fix: 500 page use variable

--- prodekoorg/views.py
+++ prodekoorg/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def handler500(request, template_name="500.html"):
-    response = render(request, "500.html")
+    response = render(request, template_name)
     response.status_code = 500
     return response
