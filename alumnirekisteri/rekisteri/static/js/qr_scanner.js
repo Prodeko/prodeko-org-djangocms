d82e61400a0b1c21909ec4f442c6e94587378976
Niklas Linnanen <nlinnanen@gmail.com>
2023-11-16 11:10:09 +0200

    use back camera in qr code reading and fix layout on mobile

--- alumnirekisteri/rekisteri/static/js/qr_scanner.js
+++ alumnirekisteri/rekisteri/static/js/qr_scanner.js
@@ -9,7 +9,7 @@ scanner.addListener('scan', function (content) {
 Instascan.Camera.getCameras()
   .then(function (cameras) {
     if (cameras.length > 0) {
-      scanner.start(cameras[0]);
+      scanner.start(cameras[cameras.length - 1]);
     } else {
       console.error('No cameras found.');
     }
