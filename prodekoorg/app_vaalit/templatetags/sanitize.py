749231d97bf6842f076e765fc8a8ee444f32b3ee
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2023-11-18 19:21:52 +0200

    Add more whitelisted tags and attributes

--- prodekoorg/app_vaalit/templatetags/sanitize.py
+++ prodekoorg/app_vaalit/templatetags/sanitize.py
@@ -5,24 +5,42 @@ from bs4 import BeautifulSoup, Comment
 
 register = Library()
 
+acceptable_elements = set(('a', 'abbr', 'acronym', 'address', 'area', 'b', 'big',
+      'blockquote', 'br', 'button', 'caption', 'center', 'cite', 'code', 'col',
+      'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl', 'dt', 'em',
+      'font', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 
+      'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu', 'ol', 
+      'p', 'pre', 'q', 's', 'samp', 'small', 'span', 'strike',
+      'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
+      'thead', 'tr', 'tt', 'u', 'ul', 'var'))
+
+acceptable_attributes = set(('abbr', 'accept', 'accept-charset', 'accesskey',
+  'action', 'align', 'alt', 'axis', 'border', 'cellpadding', 'cellspacing',
+  'char', 'charoff', 'charset', 'checked', 'cite', 'clear', 'cols',
+  'colspan', 'color', 'compact', 'coords', 'datetime', 'dir', 
+  'enctype', 'for', 'headers', 'height', 'href', 'hreflang', 'hspace',
+  'id', 'ismap', 'label', 'lang', 'longdesc', 'maxlength', 'method',
+  'multiple', 'name', 'nohref', 'noshade', 'nowrap', 'prompt', 
+  'rel', 'rev', 'rows', 'rowspan', 'rules', 'scope', 'shape', 'size',
+  'span', 'src', 'start', 'summary', 'tabindex', 'target', 'title', 'type',
+  'usemap', 'valign', 'value', 'vspace', 'width'))
+
 @register.filter
 @stringfilter
 def sanitize(value):
     rjs = r'[\s]*(&#x.{1,7})?'.join(list('javascript:'))
     rvb = r'[\s]*(&#x.{1,7})?'.join(list('vbscript:'))
     re_scripts = re.compile('(%s)|(%s)' % (rjs, rvb), re.IGNORECASE)
-    validTags = 'a b blockquote br code dd del details div dl dt em h1 h2 h3 h4 h5 h6 hr i img ins kbd li ol p pre q rp rt ruby s samp span strike strong sub summary sup table tbody td tfoot th thead tr tt ul var'.split()
-    validAttrs = 'href src width height'.split()
     soup = BeautifulSoup(value, features='lxml')
     for comment in soup.findAll(text=lambda text: isinstance(text, Comment)):
         # Get rid of comments
         comment.extract()
     for tag in soup.findAll(True):
-        if tag.name not in validTags:
+        if tag.name not in acceptable_elements:
             tag.hidden = True
         attrs = tag.attrs
         for attr, val in attrs.items():
-            if attr in validAttrs:
+            if attr in acceptable_attributes:
                 val = re_scripts.sub('', val) # Remove scripts (vbs & js)
                 tag.attrs[attr] = val
 
