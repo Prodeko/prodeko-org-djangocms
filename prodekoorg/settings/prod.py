bb3f5bf95047e6a4c0459ae61dbc26c871c77894
Risto Virtaharju <rvirtaha@hotmail.com>
2026-08-10 17:15:57 +0300

    style: fix ruff lint

--- prodekoorg/settings/prod.py
+++ prodekoorg/settings/prod.py
@@ -2,7 +2,14 @@ from .base import *
 
 DEBUG = False
 # 127.0.0.1 is the deploy health check: deploy.sh curls gunicorn on loopback.
-ALLOWED_HOSTS = ["prodeko.org", ".prodeko.org", "prodeko.fi", ".prodeko.fi", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "prodeko.org",
+    ".prodeko.org",
+    "prodeko.fi",
+    ".prodeko.fi",
+    "0.0.0.0",
+    "127.0.0.1"
+]
 
 CORS_ALLOWED_ORIGINS = ["https://ilmo.prodeko.org", "https://browser.sentry-cdn.com"]
 
